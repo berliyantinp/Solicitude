a4471585d79cfa258fc824a6c6533fefcbc2f6fb
Berliyantin Puspaningrum <berliyantinp@gmail.com>
2022-05-08 23:02:44 +0700

    Changed the layout, item bar and adjusted the simulator to iPhone 13 Pro.

--- Solicitude/ViewController.swift
+++ Solicitude/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var morningTableview:UITableView!
+    
+    var tasks = ["Mandi","Masak","Sarapan"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        morningTableview.delegate = self
+        morningTableview.dataSource = self
+        self.morningTableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellMorning")
     }
-
-
 }
 
+extension ViewController:UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellMorning", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
